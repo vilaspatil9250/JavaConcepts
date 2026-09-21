f8a33280fd9e75e656abcdc6089adb6db69f8322
vilaspatil9250 <53150772+vilaspatil9250@users.noreply.github.com>
2026-09-21 20:54:34 +0530

    Create TestPage.java

---
+++ src/accessModifiers01/TestPage.java
@@ -0,0 +1,14 @@
+package accessModifiers01;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+	public  void method01() {
+		System.out.println("Method 01");
+
+	}
+}
