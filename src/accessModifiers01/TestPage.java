63a0d61d2ecf231f3a4a13b963d650c49442010a
Vilas Patil <vilaspatil9250@gmail.com>
2026-09-21 21:01:48 +0530

    added test page

---
+++ src/accessModifiers01/TestPage.java
@@ -0,0 +1,14 @@
+package accessModifiers01;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+	public  void methodLocal() {
+		System.out.println("Method - local branch ");
+
+	}
+}
